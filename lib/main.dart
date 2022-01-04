014dc09e0a56148e8399de85847c2ddae7505171
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2022-01-04 19:33:08 -0400

    Interface do APP

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jokenpo/jogo.dart';
 
 void main(){
-  
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: true,
+  ));
 }
 
